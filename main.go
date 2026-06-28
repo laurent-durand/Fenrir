5ce532f6ee1594d17d3bf1fadeeea241e03eeab8
laurent-durand <2.97517149e+08+laurent-durand@users.noreply.github.com>
2026-06-28 23:37:52 +0000

    Implement core chaos simulation logic in Go

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello from Fenrir")
+	fmt.Println("--- Fenrir: Chaos Engineering Tool (Go) ---")
+	fmt.Println("Simulating system stress...")
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		for {
+			fmt.Printf("[%s] Fenrir is hungry: consuming 100MB RAM (simulated)\n", time.Now().Format("15:04:05"))
+			time.Sleep(2 * time.Second)
+		}
+	}()
+
+	<-stop
+	fmt.Println("\nFenrir is satiated. Shutting down.")
 }
